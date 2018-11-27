cae76280f85f593aec55e6c46198678f93f08394
Leonid_PN <poluyan939@yandex.ru>
2018-11-27 13:29:30 +0400

    laba1

---
+++ IPProject/Controllers/InformationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPProject.Controllers
+{
+    public class InformationController : Controller
+    {
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ IPProject/Controllers/NewsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPProject.Controllers
+{
+    public class NewsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Interesting()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult NewsInCategory()
+        {
+            return View();
+        }
+    }
+}
