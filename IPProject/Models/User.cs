0b9288e765b2991d89ccd6a6096f80ab6e1e5eec
Leonid_PN <poluyan939@yandex.ru>
2018-11-27 15:06:49 +0400

    laba2

---
+++ IPProject/Models/User.cs
@@ -0,0 +1,14 @@
+﻿
+namespace IPProject.Models
+{
+    public class User
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
